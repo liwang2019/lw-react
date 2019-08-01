b3c3d547d7f8b185cbb35c1d5c7378645a1cdaa4
li.wang <2951708609@qq.com>
2019-08-01 18:19:16 +0800

    first version

--- lwblog/src/App.js
+++ lwblog/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './common/header';
+import Home from './pages/home';
+import Detail from './pages/detail/loadable.js';
+import Login from './pages/login';
+import Write from './pages/write';
+import store from './store';
+import { GlobalFontStyle } from './statics/iconfont/iconfont';
+import { GlobalStyle } from './style';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      /* App is enclosed by Provider. Provider provides a store that make sure that all components in it can use this store */
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            {/* this shows how you use GlobalStyle in styled component*/}
+            <GlobalFontStyle />
+            <GlobalStyle />
+            <Header />
+            {/* exact means only the path is exactly what is written here, then the component will be used*/}
+            <Route path='/' exact component={Home}></Route>
+            <Route path='/login' exact component={Login}></Route>
+            <Route path='/write' exact component={Write}></Route>
+            <Route path='/detail/:id' exact component={Detail}></Route>
+          </div>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
 }
 
 export default App;
