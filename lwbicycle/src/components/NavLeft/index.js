e82dbed30346ad7c1d4ed638756ae26cedf5e812
li.wang <2951708609@qq.com>
2019-08-10 10:23:24 +0800

    second commit

---
+++ lwbicycle/src/components/NavLeft/index.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Menu, Icon } from 'antd';
+import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { switchMenu } from './../../redux/action'
+import MenuConfig from './../../config/menuConfig'
+import './index.less'
+const SubMenu = Menu.SubMenu;
+class NavLeft extends React.Component {
+    state = {
+        currentKey:''
+    }
+    handleClick = ({ item ,key})=>{
+        const { dispatch } = this.props;
+        dispatch(switchMenu(item.props.title))
+        this.setState({
+            currentKey:key
+        })
+    }
+    componentWillMount(){
+        const menuTreeNode = this.renderMenu(MenuConfig);
+        let currentKey = window.location.hash.replace(/#|\?.*$/g, '');
+        this.setState({
+            currentKey,
+            menuTreeNode
+        })
+    }
+    // 菜单渲染
+    renderMenu =(data)=>{
+        return data.map((item)=>{
+            if(item.children){
+                return (
+                    <SubMenu title={item.title} key={item.key}>
+                        { this.renderMenu(item.children)}
+                    </SubMenu>
+                )
+            }
+            return <Menu.Item title={item.title} key={item.key}>
+                <NavLink to={item.key}>{item.title}</NavLink>
+            </Menu.Item>
+        })
+    }
+    render() {
+        return (
+            <div>
+                <div className="logo">
+                    <img src="/assets/logo-ant.svg" alt=""/>
+                    <h1>Imooc MS</h1>
+                </div>
+                <Menu
+                    onClick={this.handleClick}
+                    selectedKeys={this.state.currentKey}
+                    theme="dark"
+                >
+                    { this.state.menuTreeNode }
+                </Menu>
+            </div>
+        );
+    }
+}
+export default connect()(NavLeft);
