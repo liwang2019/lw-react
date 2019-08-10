5e5cf1c5c42731243780c1c2cbf27bba8c3084ac
li.wang <2951708609@qq.com>
2019-08-10 10:22:34 +0800

    second commit

--- lwbicycle/src/index.js
+++ lwbicycle/src/index.js
@@ -4,7 +4,13 @@ import './index.css';
 import Life from './pages/demo/Life';
 import Admin from './admin'
 import Router from './router'
+import { Provider } from 'react-redux'
+import configureStore from './redux/store/configureStore'
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Router />, document.getElementById('root'));
+const store = configureStore();
+ReactDOM.render(
+    <Provider store={store}>
+        <Router/>
+    </Provider>    
+    , document.getElementById('root'));
 registerServiceWorker();
